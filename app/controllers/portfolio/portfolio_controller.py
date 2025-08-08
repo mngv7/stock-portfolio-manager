440fb38d562f5ce9f83eac5521633569acea7b7b
mngv7 <zackariya.taylor@gmail.com>
2025-08-08 23:54:21 +1000

    Template code for controller and router for: users, portfolio, trades

---
+++ app/controllers/portfolio/portfolio_controller.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+def get_portfolio_by_user_id(user_id: int):
+    pass
---
+++ app/routes/portfolio/portfolio_router.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+import app.controllers.portfolio.portfolio_controller as pc
+
+router = APIRouter()
+
+router.get("/users/{user_id}/portfolio")(pc.get_portfolio_by_user_id)
