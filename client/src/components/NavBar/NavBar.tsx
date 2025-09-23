36a2de237fe17a43b6a98957a52ee3dd3c2202b4
mngv7 <zackariya.taylor@gmail.com>
2025-09-23 22:44:56 +1000

    upgrade to premium button if not already premium

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,24 @@
 import { NavLink } from "react-router-dom";
 import './NavBar.css'
-import { upgradeToPremiumUser } from "../../api/auth";
+import { decodeJwt, upgradeToPremiumUser } from "../../api/auth";
+import { useState } from "react";
 
 function NavBar() {
     const token = localStorage.getItem("jwt");
+    const [isPremiumUser, setIsPremiumUser] = useState(false);
 
     const clearJwt = () => {
         localStorage.setItem("jwt", "");
+    }
+
+    const verifyPremiumUser = async () => {
+        if (!token) return;
+        const response = await decodeJwt(token);
+        if (response["cognito:groups"].includes("premium-user")) {
+            setIsPremiumUser(true);
+        } else {
+            setIsPremiumUser(false);
+        }
     }
 
     const handleUpgrade = async () => {
@@ -16,12 +28,16 @@ function NavBar() {
         console.log(response);
     }
 
+    verifyPremiumUser();
+
     return (
         <nav className='navbar'>
             <NavLink to='/app/portfolio'>
                 Dashboard
             </NavLink>
-            <button onClick={handleUpgrade}>Upgrade</button>
+            
+            {!isPremiumUser && <button onClick={handleUpgrade}>Upgrade</button>}
+            
             <div className="navbar-right">
                 <NavLink to='/' onClick={clearJwt}>
                     Log Out
